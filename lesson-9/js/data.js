a7d845b858967df5a10a47bbc8437d75317cb1ac
iloveronilo <iloveronilo@gmail.com>
2022-03-05 16:34:51 +0800

    lesson 09 update

---
+++ lesson-9/js/data.js
@@ -0,0 +1,89 @@
+let data = `{
+    "companies": [
+        {
+            "name": "Bataan Government",
+            "address": "Capitol Compound San Jose Balanga City Bataan Philippines 2100",
+            "phone": "(047) 237 3488",
+            "website": "https://www.bataan.gov.ph/",
+            "imageUrl": "images/imageUrl/bataan.png" 
+        },
+        {
+            "name": "Camaya Beach Resort",
+            "address": "Mariveles, Bataan",
+            "phone": " (+63) 917 520 6192 / (+63) 917 543 0560",
+            "website": "https://www.camayacoast.com/",
+            "imageUrl": "https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D118644859543004&imgrefurl=https%3A%2F%2Fwww.facebook.com%2FCamayaCoastNow%2F&tbnid=PP9PPz05W4UUfM&vet=12ahUKEwjDt9_Csa72AhVLEKYKHVJMCx0QMygHegQIARAv..i&docid=fP9o1VIAs2j0QM&w=500&h=500&itg=1&q=camaya%20beach%20resort&hl=en&ved=2ahUKEwjDt9_Csa72AhVLEKYKHVJMCx0QMygHegQIARAv"
+        },
+        {
+            "name": "",
+            "address": "",
+            "phone": "",
+            "website": "",
+            "imageUrl": ""
+        },
+        {
+            "name": "",
+            "address": "",
+            "phone": "",
+            "website": "",
+            "imageUrl": ""
+        },
+        {
+            "name": "",
+            "address": "",
+            "phone": "",
+            "website": "",
+            "imageUrl": ""
+        },
+        {
+            "name": "",
+            "address": "",
+            "phone": "",
+            "website": "",
+            "imageUrl": ""
+        },
+        {
+            "name": "",
+            "address": "",
+            "phone": "",
+            "website": "",
+            "imageUrl": ""
+        },
+        {
+            "name": "",
+            "address": "",
+            "phone": "",
+            "website": "",
+            "imageUrl": ""
+        },
+        {
+            "name": "",
+            "address": "",
+            "phone": "",
+            "website": "",
+            "imageUrl": ""
+        },
+        {
+            "name": "",
+            "address": "",
+            "phone": "",
+            "website": "",
+            "imageUrl": ""
+        },
+        {
+            "name": "",
+            "address": "",
+            "phone": "",
+            "website": "",
+            "imageUrl": ""
+        },
+        {
+            "name": "",
+            "address": "",
+            "phone": "",
+            "website": "",
+            "imageUrl": ""
+        }
+    ]
+}`;
+console.log(JSON.parse(data));
